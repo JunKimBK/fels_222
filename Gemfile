1b499c3d0924fd0cf5d02657e0f015c54d89d606
Vo Tung <vtungcntt12t1@gmail.com>
2016-12-07 10:29:51 +0700

    signup e-learning app

--- Gemfile
+++ Gemfile
@@ -1,13 +1,16 @@
 gem "rails", "~> 5.0.0", ">= 5.0.0.1"
 gem "bootstrap-sass",       "3.2.0.0"
+gem "bcrypt", "3.1.11"
 gem "sqlite3"
 gem "puma", "~> 3.0"
 gem "sass-rails", "~> 5.0"
 gem "uglifier", ">= 1.3.0"
 gem "coffee-rails", "~> 4.2"
+gem "jquery-rails"
 gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
 gem "jquery-rails"
+gem "config", "~> 1.3"
 
 group :development, :test do
   gem "byebug", platform: :mri
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,63 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :logged_in_user, only: [:index, :show]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_or_correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.user.per_page
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "welcome"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t "not_exist"
+      redirect_to signup_path
+    end
+  end
+
+  def user_params
+    params.require(:user).permit :name, :email,
+      :password, :password_confirmation
+  end
+
+  def correct_user
+    set_user
+    unless @user == current_user
+      flash[:alert] = t"not_current_user"
+      redirect_to root_path
+    end
+  end
+
+  def admin_or_correct_user
+    unless current_user.is_user?(@user) || admin?
+      flash[:danger] = t "denied"
+      redirect_to @user
+    end
+  end
+end
