99b90cbfe5cb065bb59c2068fa59bec7cce3ec9e
Vo Tung <vtungcntt12t1@gmail.com>
2016-12-16 15:20:59 +0700

    admin users

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,31 @@
+class Admin::UsersController < ApplicationController
+  before_action :logged_in_user
+  before_action :verify_admin, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page],
+      per_page: Settings.user.per_page
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "User_deleted"
+    else
+      flash[:danger] = t "User_not_deleted"
+    end
+    redirect_to admin_users_url
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :email
+  end
+
+  def verify_admin
+    @user = User.find_by_id params[:id]
+    unless current_user.is_admin?
+      flash[:danger] = t "access_denied"
+      redirect_to root_url
+    end
+  end
+end
